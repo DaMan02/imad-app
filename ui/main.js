affdc55092b681dbfa6a1ef24b3d7eaf6b76761b
Manjeet Dayal <mdayal02@gmail.com>
2017-08-06 20:03:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,13 @@ console.log('Loaded!');
 //change main text
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    var img=document.getElementById('madi');
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50)
+}
