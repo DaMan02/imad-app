48d0a5d9051dbeea7052646aa6fb44946b09cdf9
Manjeet Dayal <mdayal02@gmail.com>
2017-08-09 21:09:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,7 @@
-
-var w=0,h=0;
-myImg.onclick=function(){
-    var myImg=document.getElementById('myImg');
-    w=w+2;h=h+2;
-if(myImg && myImg.style) {
-    yourImg.style.height = w+'px';
-    yourImg.style.width = w+'px';
-}
-
+var btn=document.getElementById('btn');
+var counter=0;
+btn.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('counter');
+    span.innnerHTML=counter.toString();
 };
