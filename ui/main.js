f323c510a376285824d6860d6ab2392d0505cd19
Manjeet Dayal <mdayal02@gmail.com>
2017-08-09 21:03:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var myImg=document.getElementById('myImg');
-var w=0;
+var w=0,h=0;
 myImg.onclick=function(){
-   
+    w=w+2;h=h+2;
 if(myImg && myImg.style) {
     yourImg.style.height = w+'px';
     yourImg.style.width = w+'px';
