41351e06615c94f056006a30948c76b9b266ff81
Manjeet Dayal <mdayal02@gmail.com>
2017-08-09 21:11:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ var btn=document.getElementById('btn');
 var counter=0;
 btn.onclick=function(){
     counter=counter+1;
-    var span=document.getElementById('counter');
-    span.innnerHTML=counter.toString();
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
