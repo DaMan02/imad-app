923993e4fc9b8fd6ef30bc51078499091f6d4e12
Manjeet Dayal <mdayal02@gmail.com>
2017-08-09 20:28:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,6 +101,11 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send("No. of times visited : "counter.toString();));
+});
 app.get('/:articleName',function (req,res){
     //articleName== article-one
     //articles[articleName]=={}content object for article one
